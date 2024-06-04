94a09ab1c8db012eaacc41955b951581c4f2a9bd
Encore <imsolus69@gmail.com>
2024-06-05 00:53:58 +0700

    main changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 0;
+        int refill = 2300;
+
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+        int bonus = (refill > 1000) ? (refill / 100) : 0;
+
+        int summ = balance + refill + bonus;
+        System.out.println("Ваш баланс: " + summ);
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
     }
 }
